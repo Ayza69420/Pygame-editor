b99b2203d7ec18a748ff1cc1cb7888436eae80f4
Ayza <87999011+Ayza69420@users.noreply.github.com>
2021-12-01 10:19:16 +0300

    Update objects.py

--- main/objects.py
+++ main/objects.py
@@ -12,3 +12,16 @@ class RECT(pygame.Rect):
     def create(self):
         self.rect = pygame.draw.rect(self.main.window, (0,0,0),pygame.Rect(self.x,self.y,self.width,self.height))
 
+class TEXT:
+    def __init__(self, size, text, main, x=25, y=25, index=0):
+        self.text = text
+        self.main = main
+        self.x = x
+        self.y = y
+        self.size = size
+        self.index = index
+
+    def create(self):
+        self.obj = pygame.font.SysFont('freesansbold.ttf',self.size)
+        
+        self.main.window.blit(self.obj.render(self.text,False,(0,0,0)), (self.x, self.y))
