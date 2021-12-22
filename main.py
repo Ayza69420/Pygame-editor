20cb217f9f00758136c968d5ca203ce69890c701
Ayza <87999011+Ayza69420@users.noreply.github.com>
2021-12-22 22:27:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+try:
+    import pygame
+except ModuleNotFoundError:
+    os.system("pip install pygame")    
+
+    print("Pygame auto installation finished, you may restart the program now.")
+except ImportError:
+    os.system("pip install pygame")    
+
+    print("Pygame auto installation finished, you may restart the program now.")
+
+print("""
+| HOTKEYS  
+|
+| M = Menu  
+| Z = Redo (Contains Important stuff, one of them being settings with contain coloring, font changing, etc.)
+| Y = Undo
+| C = Copy
+| V = Paste
+| X = Cut
+|
+| OBJECTS HOTKEYS
+|
+| T = Create text T = Create text (This won't create text if you select on an existing text and hover over it, instead edit it)
+| E = Create New Rect
+| R = Remove Object
+|
+| TEXT CONTROL HOTKEYS
+|
+| Backspace = Removes text
+| Enter = Finish/End
+| Escape (esc) = Change text size 
+| Escape (esc) again = Finish/End Changing
+""")
+
+import main.main_loop
