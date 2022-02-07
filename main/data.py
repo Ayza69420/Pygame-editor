d1bd2e2e565bea496dca7f3cbfa4f32a1e0e219d
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-02-07 03:49:57 +0300

    Update data.py

--- main/data.py
+++ main/data.py
@@ -3,13 +3,15 @@ import os
 
 from main.objects import RECT, TEXT
 
+data_file = f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json"
+
 class data:
     def __init__(self, main):
         self.main = main
 
     def get_data(self):
         try:
-            with open(f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json", "r") as data:
+            with open(data_file, "r") as data:
                 data = json.loads(data.read())
 
                 if data:
@@ -19,17 +21,18 @@ class data:
                             self.main.rects.append(x)
 
                         elif i["type"] == "text":
-                            x = TEXT(i["size"],i["text"],self.main,i["x"],i["y"])
+                            x = TEXT(i["size"],i["text"],self.main,i["x"],i["y"], i["font"])
                             self.main.text.append(x)
+
         except Exception as err:
-            with open(f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json","w") as data:
+            with open(data_file,"w") as data:
                 data.write(json.dumps([]))
 
                 print("Failed to retrieve data.")
                 self.main.debug(err)
 
     def save_data(self):
-        with open(f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json", "w") as data:
+        with open(data_file, "w") as data:
             try:
                 data_to_write = []
 
@@ -39,21 +42,21 @@ class data:
 
                 if self.main.text:
                     for i in self.main.text:
-                        data_to_write.append({"size": i.size, "text": i.text, "x": i.x, "y": i.y, "type": "text"})
+                        data_to_write.append({"size": i.size, "text": i.text, "x": i.x, "y": i.y, "font": i.font, "type": "text"})
 
                 data.write(json.dumps(data_to_write))
 
                 print("Successfully saved.")
 
             except Exception as err:
-                with open(f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json","w") as data:
+                with open(data_file,"w") as data:
                     data.write(json.dumps([]))
 
                     print("Failed to save the data")
                     self.main.debug(err)
 
     def clear_data(self):
-        with open(f"{os.path.split(os.path.realpath(__file__))[0]}\\data.json", "w") as data:
+        with open(data_file, "w") as data:
             data.write(json.dumps([]))
 
             print("Successfully cleared the data")
