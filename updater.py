2f143d7156d3a17cce7ec98a4229062e93ac9536
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-02-11 14:53:41 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -6,7 +6,7 @@ path = os.path.split(os.path.realpath(__file__))[0] # Path to the current direct
 
 directory_files = [os.listdir(path), os.listdir(path+"\\main")] # Used to compare between the directory & repo files to find new files
 
-version = requests.get("https://raw.githubusercontent.com/Ayza69420/Pygame-editor/version.txt").text # Repo version
+version = requests.get("https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/version.txt").text.strip() # Repo version
 
 repo_files = requests.get("https://api.github.com/repos/Ayza69420/Pygame-editor/git/trees/main?recursive=1").json()
 
@@ -14,11 +14,11 @@ def update():
     for repo_file in repo_files["tree"]:
         if repo_file["mode"] == 100644:
             file_path = repo_file["path"]
-            content = requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/{file_path}").text
+            content = requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/{file_path}").text
             
             if file_path == "main/settings.json": # This is to not mess up the user's current set settings, just adds the new settings.
                 with open(path+file_path, "r+") as f:
-                    settings = json.loads(requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/settings.json").text)
+                    settings = json.loads(requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/main/settings.json").text)
                     user_settings = json.loads(f.read())
 
                     if len(user_settings) != len(settings):
