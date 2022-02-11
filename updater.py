3f1df43e06cc8a36607551f07a8137008ee2fb50
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-02-11 17:03:44 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -6,46 +6,43 @@ print("NOTICE: THE WHOLE PROCESS MAY TAKE LONG")
 
 path = os.path.split(os.path.realpath(__file__))[0] # Path to the current directory
 
-version = requests.get("https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/version.txt").text.strip() # Repo version
+version = requests.get("https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/version.txt").text # Repo version
 
 repo_files = requests.get("https://api.github.com/repos/Ayza69420/Pygame-editor/git/trees/main?recursive=1").json()
 
 def update():
     for repo_file in repo_files["tree"]:
-        if repo_file["mode"] == "100644":
-            file_path = repo_file["path"]
-            content = requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/{file_path}").text.strip()
+        if repo_file["mode"] == "100644" and os.path.splitext(path+"\\"+repo_file["path"])[1] in (".py", ".txt", ".json"):
+            file_path = path+"\\"+repo_file["path"]
+            content = requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/{repo_file['path']}").text
             
-            if file_path == "main/settings.json": # This is to not mess up the user's current set settings, just adds the new settings.
-                with open(path+"\\"+file_path, "w") as fw:
-                    with open(path+"\\"+file_path, "r") as fr:
-                        settings = json.loads(requests.get(f"https://raw.githubusercontent.com/Ayza69420/Pygame-editor/main/main/settings.json").text.strip())
-                        user_settings = json.loads(fr.read())
+            # Updating
+            if os.path.exists(file_path) and repo_file["path"] != "main/settings.json":
+                print(f"Updating {repo_file['path']}..")
 
-                        if len(user_settings) != len(settings):
-                            print("Updating main/settings.json..")
-                            for i in settings:
-                                if i not in user_settings:
-                                    user_settings[i] = settings[i]
+                with open(file_path, "w") as fw:
+                    fw.write(content)
 
+            # Adding
+            elif not os.path.exists(file_path):
+                print(f"Adding {repo_file['path']}..")
+                with open(file_path, "x") as f:
+                    with open(file_path, "w") as f:
+                        f.write(content) 
+            
+            # Updating settings
+            elif repo_file["path"] == "main/settings.json":
+                with open(file_path, "r") as f:
+                     user_settings = json.loads(f.read())
+
+                     if len(user_settings) != len(json.loads(content)):
+                        for i in user_settings:
+                            if i not in json.loads(content):
+                                user_settings[i] = json.loads(content)[i]
+
+                        with open(file_path, "w") as fw:
                             fw.write(user_settings)
 
-            elif os.path.exists(path+"\\"+file_path):
-                with open(path+"\\"+file_path, "w") as fw:
-                    with open(path+"\\"+file_path, "r") as fr:
-
-                        if content != fr.read():
-                            print(f"Updating {file_path}..")
-                    
-                            fw.write(content)
-
-            elif not os.path.exists(path+"\\"+file_path):
-                print(f"Adding {file_path}..")
-
-                with open(path+"\\"+file_path, "x") as f:
-                    with open(path+file_path, "w") as f:
-                        f.write(content)
-            
     with open(path+"version.txt", "w") as ver:
         ver.write(version)
 
