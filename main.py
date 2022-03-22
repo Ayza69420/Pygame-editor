6943bc37e6240f7be5ead1ac51e3b0dad450969e
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-03-22 21:46:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import os
+from os import system
+from sys import platform
+from json import loads
+
+with open("./main/settings.json") as sett:
+    auto_update = loads(sett.read())["auto_update"]
 
 try:
     import pygame
 except ModuleNotFoundError:
-    os.system("pip3 install pygame")    
+    system("pip3 install pygame")    
     print("Pygame auto installation finished, you may restart the program now.")
 except ImportError:
-    os.system("pip3 install pygame")    
+    system("pip3 install pygame")    
     print("Pygame auto installation finished, you may restart the program now.")
 
 print("Instructions can be found at: https://github.com/Ayza69420/Pygame-editor/blob/main/README.md")
 
+if auto_update:
+    if platform in ("linux", "linux2", "darwin"):
+        system("python3 updater.py")
+    else:
+        system("python updater.py")
+        
 import main.main_loop
